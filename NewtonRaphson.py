61c435b50dde0ca2dfbdc273052b6b184bcba1c3
Gabriel <electengrhume@gmail.com>
2020-02-28 18:26:44 +0100

    Two implementations of Newton-Raphson method in Python.

---
+++ NewtonRaphson.py
@@ -0,0 +1,29 @@
+"""
+Created on Thu Feb 27 17:35:29 2020
+@author: E238958
+"""
+# Determine root using Newton-Raphson method
+
+def derivative(f, x, h):
+      return (f(x+h) - f(x-h)) / (2.0*h)  # might want to return a small non-zero if ==0
+
+def function1(x):
+    return 2*x*x-5*x+1     # just a function to show it works
+
+def function2(x):
+    return x*x - 64       # just a function to show it works
+
+def solve(f, x0, h):
+    lastX = x0
+    nextX = lastX + 10 * h  # "different than lastX so loop starts OK
+    while (abs(lastX - nextX) > h):  # this is how you terminate the loop - note use of abs()
+        newY = f(nextX)                     # just for debug... see what happens
+        #print("f(", nextX, ") = ", newY)     # print out progress... again just debug
+        lastX = nextX
+        nextX = lastX - newY / derivative(f, lastX, h)  # update estimate using N-R
+    return nextX
+
+
+xFound = solve(function2, 5, 0.00001)    # call the solver
+print("Answer: x = ", xFound)        # print the result
+
---
+++ NewtonRaphson1.py
@@ -0,0 +1,36 @@
+"""
+Created on Mon Feb 24 15:37:44 2020
+@author: E238958
+"""
+# Determine root using Newton-Raphson method
+
+from scipy import misc
+
+def NewtonsMethod(f, x, epsilon=0.000001):
+    """ epsilon is the tolerance """
+    while True:
+        x1 = x - (f(x) / misc.derivative(f, x))
+        t = abs(x1 - x)
+        if t < epsilon:
+            break
+        x = x1
+    return x
+
+def func1(x):
+    return (2.0)*x**2 - (5.0)*x - 1
+    
+def func2(x):
+    return x**3 - 64.0
+
+x = 5
+
+x0 = NewtonsMethod(func2, x)
+
+print('x: ', x)
+print('Solution, x0: ', x0)
+#print("f(x0) = ", ((1.0/4.0)*x0**3+(3.0/4.0)*x0**2-(3.0/2.0)*x0-2 ))
+
+#eqn = [1, 0, -1, -1]
+
+#print(NewtonsMethod(x0, 1))
+
